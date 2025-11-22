bb1d74dfe9aa6943744e26b4b297f946debac535
estebanifts24 <18389755@ifts24>
2025-11-21 21:15:34 -0300

    controles en usuarios

--- src/routers/usuarios.router.js
+++ src/routers/usuarios.router.js
@@ -8,6 +8,7 @@ const router = Router();
 // Sólo admin gestiona usuarios
 router.get("/", authenticate, authorizeRoles("admin"), controller.getAllUsuarios);
 router.get("/:id", authenticate, authorizeRoles("admin"), controller.getUsuarioById);
+router.post("/", authenticate, authorizeRoles("admin"), controller.createUsuario);
 router.put("/:id", authenticate, authorizeRoles("admin"), controller.updateUsuario);
 router.delete("/:id", authenticate, authorizeRoles("admin"), controller.deleteUsuario);
 
