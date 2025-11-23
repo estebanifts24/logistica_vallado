a15aed2b32f9b267f57c8c471153d1d53ff8e4ef
estebanifts24 <18389755@ifts24>
2025-11-23 02:57:51 -0300

    implementando descargas con multer, en proceso

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+
 // Routers
 import authRouter from "./src/routers/auth.router.js";
 import vallasRouter from "./src/routers/vallas.router.js";
@@ -14,6 +15,8 @@ import movimientosRouter from "./src/routers/movimientos.router.js";
 import camionesRouter from "./src/routers/camiones.router.js";
 import empleadosRouter from "./src/routers/empleados.router.js";
 import usuariosRouter from "./src/routers/usuarios.router.js";
+import upload from "./src/multer.js";
+
 
 dotenv.config();
 
@@ -45,6 +48,33 @@ app.get("/", (req, res) => {
 });
 
 // ------------------------
+// Endpoint de prueba para subida de archivos (Multer)
+// ------------------------
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  // En desarrollo
+  if (req.file?.path) {
+    return res.json({
+      success: true,
+      message: "Archivo guardado LOCALMENTE",
+      path: req.file.path,
+    });
+  }
+  // En producción (Vercel)
+  if (req.file?.buffer) {
+    return res.json({
+      success: true,
+      message: "Archivo recibido en MEMORIA (VERCEL)",
+      size: req.file.size,
+      mimetype: req.file.mimetype,
+    });
+  }
+
+  res.status(400).json({ success: false, error: "No se recibió archivo" });
+});
+
+
+
+// ------------------------
 // Routers
 // ------------------------
 app.use("/api/auth", authRouter);
@@ -54,6 +84,7 @@ app.use("/api/movimientos", movimientosRouter);
 app.use("/api/camiones", camionesRouter);
 app.use("/api/empleados", empleadosRouter);
 app.use("/api/usuarios", usuariosRouter);
+
 
 // ------------------------
 // 404
