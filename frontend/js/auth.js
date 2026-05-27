0ec1a6494c7bfae838f5c1dc17d269c5d3d76153
estebanifts24 <18389755@ifts24>
2026-05-27 16:54:18 -0300

    Corregido logout  limpieza localstorage

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,10 +1,17 @@
 import { loginRequest } from "./api.js";
 
 export const login = async (email, password) => {
+
   const data = await loginRequest(email, password);
 
   // GUARDAR PERSISTENTE
   localStorage.setItem("token", data.token);
+
+  // 🔥 GUARDAR USUARIO
+  localStorage.setItem(
+    "user",
+    JSON.stringify(data.user)
+  );
 
   return data;
 };
@@ -14,5 +21,9 @@ export const getToken = () => {
 };
 
 export const logout = () => {
+
+  // 🔥 LIMPIAR SESIÓN COMPLETA
   localStorage.removeItem("token");
+
+  localStorage.removeItem("user");
 };
